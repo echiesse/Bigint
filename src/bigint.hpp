ebc40a8ee59c87fc338b40345ec78f099cb6d845
Eric Chiesse <echiesse@gmail.com>
2018-11-08 15:39:45 -0200

    Commit Inicial. Implementação básica com suporte a bigints positivos.

---
+++ src/bigint.hpp
@@ -0,0 +1,141 @@
+#ifndef __BIGINT_HPP__
+#define __BIGINT_HPP__
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+// Máximo inteiro com 32 bits (unsigned): 4294967295
+
+const int BASE = 1000000000; // Base decimal para facilitar na exibição.
+const int PART_LEN = 9;
+
+string padleft(string str, int len, char padChar = ' ')
+{
+    auto ret = str;
+    while (ret.size() < len) ret = padChar + ret;
+    return ret;
+}
+
+class bigint
+{
+    private:
+        typedef vector<unsigned int> TBuffer;
+        TBuffer buffer;
+
+    public:
+
+        // Estou implementando apenas para facilitar input de inteiros pequenos
+        // O construtor baseado em strings permite qualquer valor.
+        bigint(unsigned int val)
+        {
+            // TODO: Implementar a inicializacao completa para inteiros.
+            // Por enquanto, apenas impedir que se possa usar um número negativo ou maior que BASE
+            if (val >= BASE || val < 0) throw 1;
+            buffer.push_back(val);
+        }
+
+        bigint(const bigint& other)
+        {
+            buffer = other.buffer;
+        }
+
+        // Este é o construtor que realmente interessa. Usuários podem inserir qualquer valor através deste construtor
+        bigint(string val)
+        {
+            for (int i = val.size(); i > 0; i -= PART_LEN )
+            {
+                string part = val.substr(max(i - PART_LEN, 0), min(i, PART_LEN));
+                auto p = stoi(part);
+                if (p < 0) throw 1;
+                buffer.push_back(p);
+            }
+        }
+
+        bigint operator+(const bigint& other) const
+        {
+            auto ret = bigint(0);
+            bufferSum(&ret.buffer, this->buffer, other.buffer);
+            return ret;
+        }
+
+        bigint operator++(int)
+        {
+            bufferSum(&this->buffer, this->buffer, bigint(1).buffer);
+            return *this;
+        }
+
+        TBuffer* bufferSum(TBuffer *res, const TBuffer& b1, const TBuffer& b2) const
+        {
+            auto n = max(b1.size(), b2.size());
+            *res = b1;
+            auto temp = b2;
+
+            res->resize(n, 0);
+            temp.resize(n, 0);
+
+            int carry = 0;
+            for (auto i = 0; i < n; ++i)
+            {
+                int p = (*res)[i] + temp[i] + carry;
+                carry = 0;
+                if (p >= BASE)
+                {
+                    carry = 1;
+                    p = p - BASE;
+                }
+                (*res)[i] = p;
+            }
+            if (carry > 0) res->push_back(carry);
+            return res;
+        }
+
+        // Só estou usando essa função para debug.
+        void print() const
+        {
+            for (auto i = 0; i< buffer.size(); ++i)
+            {
+                cout << buffer[i] << " ";
+            }
+            cout << endl;
+        }
+
+        string show() const
+        {
+            int i = 0;
+            string ret = "";
+            string part;
+            bool shallPrint = false;
+            for (TBuffer::const_reverse_iterator it = buffer.rbegin(); it != buffer.rend(); ++it)
+            {
+                if (*it != 0) shallPrint = true;
+                if (shallPrint)
+                {
+                    char p[10];
+                    sprintf(p, "%i", *it);
+                    part = padleft(p, PART_LEN, '0');
+                    ret += part;
+                }
+            }
+
+            return ret;
+        }
+
+        friend ostream& operator<< (ostream& stream, const bigint& num);
+
+        int bsize() const
+        {
+            return buffer.size();
+        }
+};
+
+ostream& operator<< (ostream& stream, const bigint& num)
+{
+    stream << num.show();
+    return stream;
+}
+
+#endif /** __BIGINT_HPP__ **/
---
+++ src/test.cpp
@@ -0,0 +1,102 @@
+#include "bigint.hpp"
+
+template <typename F>
+bool throws(F& lambda)
+{
+    try{lambda();}
+    catch(int e){return true;}
+    return false;
+}
+
+template <typename T>
+void print(T val)
+{
+    cout << val << endl;
+}
+
+template <typename T>
+void tprint(string name, T actual, T expected)
+{
+    print("");
+    if (actual == expected)
+    {
+        cout << "Passed: " << name << endl;
+    }
+    else
+    {
+        cout << "Failed: " << name << endl;
+        cout << "Actual = " << actual << endl;
+        cout << "Expected = " << expected << endl;
+    }
+}
+
+void tprint(string name, string actual, string expected)
+{
+    return tprint<string>(name, actual, expected);
+}
+
+int main()
+{
+    print("Testando bigint:" );
+
+    // Instanciacao:
+    auto v1 = bigint(1);
+    tprint("Instanciacao int",
+        v1.show(), "000000001"
+    );
+
+    auto v2 = bigint("12");
+    tprint("Instanciacao string",
+        v2.show(), "000000012"
+    );
+
+    auto v3 = bigint("1000000000");
+    tprint("Instanciacao big int",
+        v3.show(), "000000001000000000"
+    );
+
+    tprint("Big int tem tamanho correto",
+        v3.bsize(), 2
+    );
+
+    tprint("Inteiro muito grande gera excecao",
+        throws([]{return bigint(4000000000);}), true
+    );
+
+    tprint("Negativo gera excecao (int)",
+        throws([]{return bigint(-1);}), true
+    );
+
+    tprint("Negativo gera excecao (string)",
+        throws([]{return bigint("-1");}), true
+    );
+
+    // Soma:
+    tprint("Soma",
+        (v1 + v2).show(), "000000013"
+    );
+
+    tprint("Soma com carry",
+        (v3 + bigint("999999999") + v1).show(), "000000002000000000"
+    );
+
+    tprint("Numeros bem grandes 1",
+        (bigint("9999999999999999999999999999999999") + v1).show(),
+        "010000000000000000000000000000000000"
+    );
+
+    tprint("Numeros bem grandes 2",
+        (
+            bigint("9000000000000000000000000000000000") +
+            bigint("1000000000000000000000000000000000")
+        ).show(),
+        "010000000000000000000000000000000000"
+    );
+
+    // Operator ++
+    tprint("Operator ++",
+        (bigint("9999999999999999999999999999999999")++).show(),
+        "010000000000000000000000000000000000"
+    );
+
+}
